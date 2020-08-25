ea41dcc2da87dc3b4eac72b9a3619ca534c9a601
zhangqc <1178995389@qq.com>
2020-08-25 18:06:16 +0800

    第一天学习springcloud

---
+++ README.md
@@ -0,0 +1,6 @@
+#####author: ZhangQianChuan
+###2020.08.25 -zhangqc
+    1.服务注册Rureka
+    2.服务提供者
+    3.Ribbon
+    4.Feign
---
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/zaqacu/hello/spring/cloud/web/admin/feign/WebAdminFeignApplication.java
@@ -0,0 +1,15 @@
+package com.zaqacu.hello.spring.cloud.web.admin.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class WebAdminFeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminFeignApplication.class, args);
+    }
+}
